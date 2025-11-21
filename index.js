ec85d9fa319f5fb2aa154a64fb056e06d46ed56f
Ritika Lodhi <ritikalodhi3@gmail.com>
2025-11-21 14:09:39 +0530

    Task-2

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 5013;
 
+const submissions = [];
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -15,10 +16,25 @@ app.get("/", (req, res) => {
   res.render("form");
 });
 app.post("/submit", (req, res) => {
-  const { name, email } = req.body;
-  res.render("result", { name, email });
+  const { name, email, password, department, gender, agree, message } = req.body;
+  
+  if (!email.includes("@")) {
+    return res.render("form", { error: "Invalid email format!" });
+  }
+
+  submissions.push({ name, email, password, department, gender, message });
+  res.render("result", { name, email, department, gender  });
 });
 
+app.get("/responses", (req, res) => {
+  res.render("responses", { responses: submissions });
+});
+
+app.post("/delete/:id", (req, res) => {
+  const index = req.params.id;
+  submissions.splice(index, 1);
+  res.redirect("/responses");
+});
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
